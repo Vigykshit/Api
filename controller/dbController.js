614ff9f5893388ede37208a65d2e7f78feeb0c99
Vigykshit katoch <katoch.vigy96@gmail.com>
2023-06-15 19:40:59 +0530

    first commit

---
+++ controller/dbController.js
@@ -0,0 +1,50 @@
+let mongo = require('mongodb');
+let {MongoClient} = require('mongodb');
+let mongoUrl= "mongodb+srv://projectAmazon:qYlDdRXfhoDkFlSA@cluster1amazon.f7agszm.mongodb.net/?retryWrites=true&w=majority";
+let client = new MongoClient(mongoUrl)
+
+async function dbConnect(){
+    await client.connect()
+}
+
+let db = client.db('AmazonApi');
+
+async function getData(colName,query){
+    let output = [];
+    try{
+        const cursor = db.collection(colName).find(query);
+        for await(const data of cursor){
+            output.push(data)
+        }
+        cursor.closed
+    } catch(err){
+        output.push({"Error":"Error in getData"})
+    }
+    return output
+}
+async function postData(colName,data){
+    let output;
+    try{
+        output = await db.collection(colName).insertOne(data)
+    }
+    catch(err){
+        output = {"response":"Error in postData"}
+    }
+    return output
+}
+
+async function updateOrder(colName,condition,data){
+    let output;
+    try{
+        output = await db.collection(colName).updateOne(condition,data)
+    } catch(err){
+        output = {"response":"Error in update data"}
+    }
+    return output
+}
+
+module.exports = {
+    dbConnect,
+    getData,
+    postData,
+}
